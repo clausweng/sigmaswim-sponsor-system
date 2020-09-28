2cdd30c8498d99d1a1e5d6e4831a777fd3ee6820
Claus Weng <cwe@forsikringogpension.dk>
2020-09-28 11:04:56 +0200

    Introducing Lombok

--- src/main/java/dk/sigmaswim/sponsorsystem/domain/Athlete.java
+++ src/main/java/dk/sigmaswim/sponsorsystem/domain/Athlete.java
@@ -1,13 +1,21 @@
 package dk.sigmaswim.sponsorsystem.domain;
 
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 @Entity
+@Getter @Setter @NoArgsConstructor
 public class Athlete {
 
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)private Long id;
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    @Setter(AccessLevel.PRIVATE)
+    private Long id;
     private String ref;
     private String teamRef;
     private String firstName;
@@ -20,64 +28,4 @@ public class Athlete {
     @ManyToOne(targetEntity = Team.class)
     @JoinColumn(name = "team_id")
     private Team team;
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getRef() {
-        return ref;
-    }
-
-    public void setRef(String ref) {
-        this.ref = ref;
-    }
-
-    public String getTeamRef() {
-        return teamRef;
-    }
-
-    public void setTeamRef(String teamRef) {
-        this.teamRef = teamRef;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public Integer getYear() {
-        return year;
-    }
-
-    public void setYear(Integer year) {
-        this.year = year;
-    }
-
-    public Gender getGender() {
-        return gender;
-    }
-
-    public void setGender(Gender gender) {
-        this.gender = gender;
-    }
-
-    public Team getTeam() {
-        return team;
-    }
-
-    public void setTeam(Team team) {
-        this.team = team;
-    }
 }
--- src/main/java/dk/sigmaswim/sponsorsystem/domain/Team.java
+++ src/main/java/dk/sigmaswim/sponsorsystem/domain/Team.java
@@ -1,34 +1,21 @@
 package dk.sigmaswim.sponsorsystem.domain;
 
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 @Entity
+@Getter @Setter @NoArgsConstructor
 public class Team {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Setter(AccessLevel.PRIVATE)
     private Long id;
     private String ref;
     private String name;
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getRef() {
-        return ref;
-    }
-
-    public void setRef(String ref) {
-        this.ref = ref;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
